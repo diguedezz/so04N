a98790dc6bb63d8b91d5819e1307da30bf6b4522
Pedro-Moreno <pedromorenoc2000@hotmail.com>
2022-09-15 20:37:21 -0300

    correções de bugs

--- PROJ1/jantar.c
+++ PROJ1/jantar.c
@@ -8,8 +8,7 @@ Pedro Moreno Campos - 321
 Problema do Jantar dos Filósofos
 */
 
-
-//para que não ocorram impasses nem starvation, protegeremos os cinco comandos seguindo a chamada think com um semáforo binário, ao invés de apenas pegar garfos, um filósofo vai realizar um down em mutex e após substituir os garfos, ele realizará um up em mutex
+// para que não ocorram impasses nem starvation, protegeremos os cinco comandos seguindo a chamada think com um semáforo binário, ao invés de apenas pegar garfos, um filósofo vai realizar um down em mutex e após substituir os garfos, ele realizará um up em mutex
 //
 
 #include <stdio.h>
@@ -18,13 +17,13 @@ Problema do Jantar dos Filósofos
 #include <time.h>
 #include <string.h>
 
-#define N           5
-#define LEFT        (i+N-1)%N
-#define RIGHT       (i+1)%N
-#define THINKING    0
-#define HUNGRY      1
-#define EATING      2
-#define TRUE        1
+#define N 5
+#define LEFT (i + N - 1) % N
+#define RIGHT (i + 1) % N
+#define THINKING 0
+#define HUNGRY 1
+#define EATING 2
+#define TRUE 1
 
 typedef pthread_mutex_t semaforo;
 int estado[N];
@@ -36,65 +35,78 @@ void eat();
 void take_forks(int i);
 void put_forks(int i);
 void test(int i);
-void filosos(int i);
+void *filosos(void *args);
 
-void think(){
+void think()
+{
     sleep(5);
 }
 
-void eat(){
+void eat()
+{
     sleep(15);
 }
 
-void filosos(void *args){
-
-    int i = long args;
-
-    while(TRUE){                //looping infinito
+void *filosos(void *args)
+{
+    int i = (long)args;
+    while (TRUE)
+    { // looping infinito
         think();
-        take_forks();
+        take_forks(i);
         eat();
-        put_forks();
+        put_forks(i);
     }
 }
 
-void take_forks(int i){             // i = nº de filosofos 
-    pthread_mutex_lock(&mutex);                   // região crítica
-    estado[i] = HUNGRY;             // registra a fome do filosofo
-    test(i);                        // testa se os garfos estao disponiveis
-    pthread_mutex_unlock(&mutex);                     // sai da região critica
-    pthread_mutex_lock(&s[i]);                    // bloqueia caso os garfos nao foram pegos
+void take_forks(int i)
+{                                 // i = nº de filosofos
+    pthread_mutex_lock(&mutex);   // região crítica
+    estado[i] = HUNGRY;           // registra a fome do filosofo
+    test(i);                      // testa se os garfos estao disponiveis
+    pthread_mutex_unlock(&mutex); // sai da região critica
+    pthread_mutex_lock(&s[i]);    // bloqueia caso os garfos nao foram pegos
 }
 
-void put_forks(int i){
-    pthread_mutex_lock(&mutex);                   // região crítica
-    if(estado[i] == EATING){
+void put_forks(int i)
+{
+    pthread_mutex_lock(&mutex); // região crítica
+    if (estado[i] == EATING)
+    {
         printf("O filósofo %d largou os garfos!\n", i);
     }
-    state[i] = THINKING;            // depois de comer o filosofo larga os garfos e volta a pensar
-    test(LEFT);                     // testa se o vizinho a esuqerda pode comer
-    test(RIGHT);                    // testa se o vizinho a esuqerda pode comer
-    pthread_mutex_unlock(&mutex);                     // sai da região crítica
+    estado[i] = THINKING;         // depois de comer o filosofo larga os garfos e volta a pensar
+    test(LEFT);                   // testa se o vizinho a esuqerda pode comer
+    test(RIGHT);                  // testa se o vizinho a esuqerda pode comer
+    pthread_mutex_unlock(&mutex); // sai da região crítica
 }
 
-void test(i){
-    if(estado[i] == HUNGRY && estado[LEFT] != EATING && estado[RIGHT] != EATING){
-        state[i] = EATING;
+void test(int i)
+{
+    if (estado[i] == HUNGRY && estado[LEFT] != EATING && estado[RIGHT] != EATING)
+    {
+        estado[i] = EATING;
         printf("O filósofo %d está comendo!\n", i);
-        up(&s[i]);
-    }else if(estado[i] == HUNGRY){
+        pthread_mutex_lock(&s[i]);
+    }
+    else if (estado[i] == HUNGRY)
+    {
         printf("O filósofo %d está esperando para comer!\n", i);
     }
 }
 
-int main(int arg, char* args[], char* envp[]){
-    
+int main(int arg, char *args[], char *envp[])
+{
+
     pthread_t filosos[N];
     pthread_mutex_init(&mutex, 0);
-    for(int i=0;i<N;i++){
-        pthread_mutex_init(&s[i],0);
+    for (int i = 0; i < N; i++)
+    {
+        pthread_mutex_init(&s[i], 0);
     }
-    for(int i=0;i<N;i++){
+
+    for (int i = 0; i < N; i++)
+    {
         pthread_mutex_destroy(&s[i]);
     }
 
--- proj1Jantar/jantarfilosofos.c
+++ proj1Jantar/jantarfilosofos.c
@@ -12,81 +12,91 @@
 /*																					*/
 /************************************************************************************/
 
-//Problema: Jantar dos Filósofos
+// Problema: Jantar dos Filósofos
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <pthread.h>					//Para resolver este problema usaremos a biblioteca pthread, que nos permitirá a utilização de semáforos
+#include <pthread.h> //Para resolver este problema usaremos a biblioteca pthread, que nos permitirá a utilização de semáforos
 
 #define TRUE 1
 
-//definição de funções
+// definição de funções
 void *filosofoThread(void *args);
 void think(int i);
 void wait(int i);
 void eat(int i);
 
-pthread_mutex_t mutex[5] = {PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER};		//inicialização dos mutex, um para cara filosofo
+pthread_mutex_t mutex[5] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER}; // inicialização dos mutex, um para cara filosofo
 
-void think(int i){			//função que representa a ação de pensar do filosofo
-    printf("O filósofo %d está pensando.\n", i+1);
+void think(int i)
+{ // função que representa a ação de pensar do filosofo
+    printf("O filósofo %d está pensando.\n", i + 1);
     sleep(5);
 }
 
-void wait(int i){			//função que representa a ação de esperar do filosofo
-    printf("O filósofo %d está esperando por garfos.\n", i+1);
+void wait(int i)
+{ // função que representa a ação de esperar do filosofo
+    printf("O filósofo %d está esperando por garfos.\n", i + 1);
 }
 
-void eat(int i){			//função que representa a ação de comer do filosofo
-    printf("O filósofo %d está comendo.\n", i+1);
+void eat(int i)
+{ // função que representa a ação de comer do filosofo
+    printf("O filósofo %d está comendo.\n", i + 1);
     sleep(15);
 }
 
-void *filosofoThread(void *args){
+void *filosofoThread(void *args)
+{
 
     int i = *((int *)args);
     free((int *)args);
-	int left, right;
+    int left, right;
 
     left = i;
     right = (i + 1) % 5;
 
-    while (TRUE){				//looping infinito
+    while (TRUE)
+    { // looping infinito
         think(i);
         wait(i);
-        while (TRUE){
+        while (TRUE)
+        {
             if (pthread_mutex_trylock(&mutex[left]) != 0)
                 continue;
 
-            if (pthread_mutex_trylock(&mutex[right]) != 0){
+            if (pthread_mutex_trylock(&mutex[right]) != 0)
+            {
                 pthread_mutex_unlock(&mutex[left]);
                 continue;
             }
 
-            printf("O filosofo %d conseguiu os dois garfos.\n", i+1);
+            printf("O filosofo %d conseguiu os dois garfos.\n", i + 1);
             break;
         }
-        eat(x);
+        eat(i);
         pthread_mutex_unlock(&mutex[left]);
         pthread_mutex_unlock(&mutex[right]);
     }
     pthread_exit(NULL);
 }
 
-int main(){
+int main()
+{
 
     pthread_t thread[5];
 
-    for (int i = 0; i < 5; i++){
-        int *j = malloc(sizeof(int));
+    for (int i = 0; i < 5; i++)
+    {
+        int *j = malloc(sizeof(int)); // setando o espaço de memória
         *j = i;
-        printf("Filósofo %d sentou na mesa.\n", i+1);
-        pthread_create(&thread[i], NULL, filosofoThread, void* j);
+        printf("Filósofo %d sentou na mesa.\n", i + 1);
+        pthread_create(&thread[i], NULL, filosofoThread, (void *)j);
     }
 
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++)
+    {
         pthread_join(thread[i], NULL);
     }
 
